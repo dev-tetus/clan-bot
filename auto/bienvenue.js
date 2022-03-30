2d01d996553056812ad5951d20ce217abb99ca74
Teo Roca <teo.roca798@gmail.com>
2022-03-30 06:53:37 +0200

    add automatic messages

--- auto/bienvenue.js
+++ auto/bienvenue.js
@@ -1,114 +1,210 @@
 const { MessageEmbed, Message } = require('discord.js');
 
-module.exports = async (client) =>{
-   
-    const embed = new MessageEmbed()
-	.setColor('#c7c7c7')
-	.setTitle(`**               ** BIENVENUE DANS LE SERVEUR DU CLAN SN3T`)
-	.setDescription(`Salut et bienvenue dans notre famille, avant de commencer nous souhaitons savoir un peu plus sur toi afin de pouvoir te donner les rôles appropriés dans le serveur.`)
-    .addField(`** \n**Es-tu déjà membre du clan SN3T?`,"** **",false)
-    
-    const msg = {
-        "embeds":[embed],
+module.exports = async (client) => {
+    const server = await client.guilds.fetch('957059703458902016')
+    const channelCommandes = await server.channels.cache.find(r => r.name == 'commandes')
+    console.log(server);
+
+    const embedBienvenu = new MessageEmbed()
+        .setColor('#c7c7c7')
+        .setTitle(`**               ** BIENVENUE DANS LE SERVEUR DU CLAN SN3T`)
+        .setDescription(`Salut et bienvenue dans notre famille, avant de commencer nous souhaitons savoir un peu plus sur toi afin de pouvoir te donner les rôles appropriés dans le serveur.`)
+        .addField(`** \n**Es-tu déjà membre du clan SN3T?`, "** **", false)
+
+    const msgBienvenu = {
+        "embeds": [embedBienvenu],
         "components": [
-        {
-            "type": 1,
-            "components": [
             {
-                "style": 1,
-                "label": `Oui`,
-                "custom_id": `1`,
-                "disabled": false,
-                "emoji": {
-                "id": null,
-                "name": `👍`
-                },
-                "type": 2
-            },
-            {
-                "style": 1,
-                "label": `Non`,
-                "custom_id": `0`,
-                "disabled": false,
-                "emoji": {
-                "id": null,
-                "name": `👎`
-                },
-                "type": 2
+                "type": 1,
+                "components": [
+                    {
+                        "style": 1,
+                        "label": `Oui`,
+                        "custom_id": `1`,
+                        "disabled": false,
+                        "emoji": {
+                            "id": null,
+                            "name": `👍`
+                        },
+                        "type": 2
+                    },
+                    {
+                        "style": 1,
+                        "label": `Non`,
+                        "custom_id": `0`,
+                        "disabled": false,
+                        "emoji": {
+                            "id": null,
+                            "name": `👎`
+                        },
+                        "type": 2
+                    }
+                ]
             }
-            ]
-        }
         ],
 
     }
 
-
-    const channel = await client.channels.fetch('957073975891091486')
-    const messages = await channel.messages.fetch()
-
-    if (messages.size === 0){
-        channel.send(msg);
+    const msgDescription = {
+        "embeds": [new MessageEmbed({
+            "title": "**                                          **   [DESCRIPTION SERVEUR]",
+            "color": 9765892,
+            "description": "Bienvenu dans le serveur, ici tu pourras consulter la structure du serveur ainsi que les intéractions possibles pour ton rôle.\n**\n**",
+            "timestamp": "",
+            "author": {
+                "name": ""
+            },
+            "image": {},
+            "thumbnail": {},
+            "footer": {},
+            "fields": [
+                {
+                    "name": "🌐  __Général__",
+                    "value": `** **\nDans le serveur, l'utilisation des commandes de ${client.user} se fera dans le channel ${channelCommandes}, l'utilisation des commandes dans n'importe quel autre channel ne donnera pas de résultats.\n\nConsultez le channel ${await server.channels.cache.find(r => r.name == 'guide')} dans #Bot pour avoir plus d'informations sur l'utilisation des commandes de ${client.user}\n**\n**`
+                },
+                {
+                    "name": "☕  __Pour les invités__",
+                    "value": `**\n**__Tous les channels__ sont visibles mais pas tous sont accéssibles a l'intéraction ou même aux historiques de ceux-là.\n\nLa catégorie **${await server.channels.fetch('957806046888484864')}** est dédié à vous, crée pour que vous puissez discuter non seulement avec nous mais entre ${server.roles.cache.find(r => r.name == 'Invité')}.\n\nDans cette catégorie vous retrouverez le channel ${await server.channels.fetch('957961040405094410')} pour engager une conversation et ${await server.channels.fetch('957962440312782898')} pour vous annoncer si jamais vous désirez nous rejoindre.\n**\n**`
+                },
+                {
+                    "name": "⚔️  __Pour les membres du clan__",
+                    "value": `**\n**Tous les channels sont accésibles pour vous à l'intéraction, comme vous pouvez constater, il existe une catégorie par évènement.\n\nIl existe aussi un channel ${await server.channels.fetch('957074488137240587')} par catégorie dans lequel nous posterons des infos importantes à lire dans le contexte de l'évènement.\n\n${client.user} utilisera aussi ces channels pour envoyer des messages automatiques avec des infos sur l'évènement`
+                }
+            ]
+        },
+            {
+                "color": 0,
+                "timestamp": "",
+                "author": {},
+                "image": {},
+                "thumbnail": {},
+                "footer": {},
+                "fields": []
+            })]
     }
-   
 
-    
+    const msgGuideCommandes = {
+        "embeds": [new MessageEmbed({
+            "title": "**                                  **👨‍💻 DESCRIPTION COMMANDES 👩‍💻",
+            "color": 9765892,
+            "description": `Afin de faciliter la gestion du clan et du serveur nous avons développé notre petit ${client.user}, il est très jeune donc très fragile, soyez sympa avec.\n\n${client.user} nous permettra d'automatiser des tâches en fonction de la situation du clan comme envoyer des rappels de temps restant de GDC, de pourcentage de destruction...\n\nIl organisera des votes en fonction de la situation du clan afin de savoir qui est prêt à s'engager à participer dans l'évènement à venir\n\n⚠️ __RAPPEL! Ces commandes seront que utilisables dans le channel ${channelCommandes}__\n\n\n*En cours de développement*\n\n`,
+            "timestamp": "",
+            "author": {
+                "name": ""
+            },
+            "image": {},
+            "thumbnail": {},
+            "footer": {},
+            "fields": [
+                {
+                    "name": "__*CMD*__",
+                    "value": '\u200B',
+                    "inline": true
+                },
+                {
+                    "name": "__*Description*__",
+                    "value": '\u200B',
+                    "inline": true
+                },
+                {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    "name": "/donnations",
+                    "value": '\u200B',
+                    "inline": true
+                },
+                {
+                    "name": "Affiche la moyenne de dons par joeurs pendant la période actuelle ",
+                    "value": '\u200B',
+                    "inline": true
+                },
+                {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: '/recevoir',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: `Envoie un message dans un *besoin-de-troupes* après avoir répondu les différentes questions de __${client.user.username}__ à propos de ta demande`,
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: '/donner',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: `Envoie un message dans un *besoin-de-troupes* après avoir répondu les différentes questions de __${client.user.username}__ à propos de ton offre.`,
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: '/points',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: 'Affiche le nombre de trophées total actuel dans le clan',
+                    value: '\u200B',
+                    inline: true
+                },
+                {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true
+                },
+
+
+            ]
+        })]
+
+    }
+
+    const channelBienvenu = await client.channels.fetch('957073975891091486')
+    const messagesBienvenu = await channelBienvenu.messages.fetch()
+
+
+    const channelDescription = await client.channels.fetch('957077823720423516')
+    const messagesDescription = await channelDescription.messages.fetch()
+
+    const channelGuideCommandes = await client.channels.fetch('957089459072028672')
+    const messagesGuideCommandes = await channelGuideCommandes.messages.fetch()
+
+    if (messagesBienvenu.size === 0) {
+        channelBienvenu.send(msgBienvenu);
+    }
+    if (messagesDescription.size == 0) {
+        await channelDescription.send(msgDescription);
+    }
+    else {
+        await channelDescription.lastMessage.delete()
+        await channelDescription.send(msgDescription);
+    }
+    if (messagesGuideCommandes.size == 0) {
+        await channelGuideCommandes.send(msgGuideCommandes);
+    }
+    else {
+        await channelGuideCommandes.lastMessage.delete()
+        await channelGuideCommandes.send(msgGuideCommandes);
+    }
 
 
 }
-
-
-
-
-
-
-
-
-// await lib.discord.channels['@0.3.0'].messages.create({
-//   "channel_id": `${context.params.event.channel_id}`,
-//   "content": "",
-//   "tts": false,
-//   "components": [
-//     {
-//       "type": 1,
-//       "components": [
-//         {
-//           "style": 1,
-//           "label": `Oui`,
-//           "custom_id": `1`,
-//           "disabled": false,
-//           "emoji": {
-//             "id": null,
-//             "name": `👍`
-//           },
-//           "type": 2
-//         },
-//         {
-//           "style": 1,
-//           "label": `Non`,
-//           "custom_id": `0`,
-//           "disabled": false,
-//           "emoji": {
-//             "id": null,
-//             "name": `👎`
-//           },
-//           "type": 2
-//         }
-//       ]
-//     }
-//   ],
-//   "embeds": [
-//     {
-//       "type": "rich",
-//       "title": `**Negrita** BIENVENUE DANS LE SERVEUR DU CLAN SN3T`,
-//       "description": `Salut et bienvenue dans notre famille, avant de commencer nous souhaitons savoir un peu plus sur toi afin de pouvoir te donner les rôles appropriés dans le serveur.`,
-//       "color": 0xc7c7c7,
-//       "fields": [
-//         {
-//           "name": `Es-tu déjà membre du clan SN3T?`,
-//           "value": "\u200B",
-//           "inline": true
-//         }
-//       ]
-//     }
-//   ]
-// });

51e4eadd39039d1c93da577d99d32f2207f66031
Teo Roca <teo.roca798@gmail.com>
2022-11-05 14:11:07 +0100

    Refactor to work on prep day and war day

--- bot/messages/inWar/clanWarLeagueInWar.js
+++ bot/messages/inWar/clanWarLeagueInWar.js
@@ -1,22 +1,21 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = (data, inWar=true) => {
+module.exports = (data, inWar) => {
+    
+    const embed = new MessageEmbed().setColor('#c7c7c7')
+    const today = new Date();
     const startTime = new Date(parseInt(data.endTime.substr(0, 4)),
         parseInt(data.endTime.substr(4, 2)) - 1,
         parseInt(data.endTime.substr(6, 2)),
         parseInt(data.endTime.substr(9, 2)) + 2,
         parseInt(data.endTime.substr(11, 2)),
-        parseInt(data.endTime.substr(13, 2))
-    )
-    const today = new Date();
+        parseInt(data.endTime.substr(13, 2)))
     const days = parseInt((startTime - today) / (1000 * 60 * 60 * 24));
     const hours = parseInt(Math.abs(startTime - today) / (1000 * 60 * 60) % 24);
     const minutes = parseInt(Math.abs(startTime.getTime() - today.getTime()) / (1000 * 60) % 60);
-    const seconds = parseInt(Math.abs(startTime.getTime() - today.getTime()) / (1000) % 60);
+    if (inWar){
+            
 
-    const embed = new MessageEmbed()
-        .setColor('#c7c7c7')
-        if (inWar){
             embed.setDescription(`***La Ligue de Clans est en cours...***\n\n⌛**${days > 0 ? days + ' jour(s) ' : ''}${hours > 0 ? hours + ' heure(s) et ' : ''} ${minutes > 0 || (minutes == 0 && hours > 0) ? minutes + ' minute(s)' : ''}\n\n**\n\n`)
             .setTitle(`[PHASE GUERRE] ** ** ⚔ LDC J-${data.day} ⚔ ** ** SN3T ⭐ ${data.stars} - ${data.enemyStars} ⭐ ${data.opponent}`)
             .setFields([
